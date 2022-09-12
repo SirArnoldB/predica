5b0eb8289df6279ddba67ecc0f5d4d4376181959
Olunusi Best <olunusibest@gmail.com>
2022-09-11 20:13:30 -0500

    adds ml models

---
+++ predica/utils/ml_models.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+from fbprophet import Prophet
+
+def housing_prices_model(data, num_years):
+    model = Prophet()
+    data = np.array(data)
+    df = pd.DataFrame(data, columns=['ds', 'y'])
+    df['ds'] = pd.to_datetime(df.ds, format='%Y')
+    model.fit(df)
+    future = model.make_future_dataframe(num_years, freq='Y')
+    forecast = model.predict(future)
+    #forecast = forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
+    return forecast.values.tolist()
